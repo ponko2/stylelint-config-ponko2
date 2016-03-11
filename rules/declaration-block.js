e896dfe469cc24bdb073f4dceb2eb29b429de6d3
Takahito Nakano <kano@ponko2.jp>
2016-03-12 00:40:18 +0900

    ルールの名称変更を反映

--- rules/declaration-block.js
+++ rules/declaration-block.js
@@ -2,8 +2,14 @@
 
 module.exports = {
   rules: {
-    // Disallow single-line declaration blocks.
-    'declaration-block-no-single-line': null,
+    // Disallow duplicate properties within declaration blocks.
+    'declaration-block-no-duplicate-properties': true,
+
+    // Disallow shorthand properties that override related longhand properties within declaration blocks.
+    'declaration-block-no-shorthand-property-overrides': true,
+
+    // Specify the order of properties within declaration blocks.
+    'declaration-block-properties-order': null,
 
     // Require a newline or disallow whitespace after the semicolons of declaration blocks.
     'declaration-block-semicolon-newline-after': 'always-multi-line',
@@ -19,5 +25,8 @@ module.exports = {
 
     // Limit the number of declaration within single line declaration blocks.
     'declaration-block-single-line-max-declarations': 1,
+
+    // Require or disallow a trailing semicolon within declaration blocks.
+    'declaration-block-trailing-semicolon': 'always',
   }
 };
--- rules/rule.js
+++ rules/rule.js
@@ -8,21 +8,9 @@ module.exports = {
       ignore: ['after-comment'],
     }],
 
-    // Disallow duplicate properties within rules.
-    'rule-no-duplicate-properties': true,
-
-    // Disallow shorthand properties that override related longhand properties.
-    'rule-no-shorthand-property-overrides': true,
-
     // Require or disallow an empty line before non-nested rules.
     'rule-non-nested-empty-line-before': ['always-multi-line', {
       ignore: ['after-comment'],
     }],
-
-    // Specify the order of properties within rules.
-    'rule-properties-order': null,
-
-    // Require or disallow a trailing semicolon within rules.
-    'rule-trailing-semicolon': 'always',
   }
 };
