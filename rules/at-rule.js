13c7099e4c881ca1c3ad43d2e63fd158d05c1490
Takahito Nakano <kano@ponko2.jp>
2016-03-25 12:05:44 +0900

    ルールの説明文を更新

--- rules/at-rule.js
+++ rules/at-rule.js
@@ -2,13 +2,13 @@
 
 module.exports = {
   rules: {
-    // Require or disallow an empty line before @rules.
+    // Require or disallow an empty line before at-rules.
     'at-rule-empty-line-before': ['always', {
       except: ['blockless-group', 'first-nested'],
       ignore: ['after-comment'],
     }],
 
-    // Disallow vendor prefixes for @rules.
+    // Disallow vendor prefixes for at-rules.
     'at-rule-no-vendor-prefix': true,
   }
 };
