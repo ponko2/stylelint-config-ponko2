da39ab9300df5cf33bf625e7dd6ac4a1de5a55db
Takahito Nakano <kano@ponko2.jp>
2016-04-11 21:31:47 +0900

    stylelint v5.4.0 追加分のルール設定

--- rules/unit.js
+++ rules/unit.js
@@ -5,6 +5,9 @@ module.exports = {
     // Specify a blacklist of disallowed units.
     'unit-blacklist': null,
 
+    // Disallow unknown units.
+    'unit-no-unknown': true,
+
     // Specify a whitelist of allowed units.
     'unit-whitelist': null,
   }
