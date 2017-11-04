ad52ae2fb98d1254d321b47be94588a981f7b481
Takahito Nakano <kano@ponko2.jp>
2017-11-05 03:51:42 +0900

    テストをjestに変更

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,36 +1,77 @@
-import fs        from 'fs';
-import config    from '../';
-import stylelint from 'stylelint';
-import test      from 'ava';
+const fs = require('fs');
+const config = require('../');
+const stylelint = require('stylelint');
 
 const validScss = fs.readFileSync('./__tests__/valid.scss', 'utf-8');
 const invalidScss = fs.readFileSync('./__tests__/invalid.scss', 'utf-8');
 
-test('no warnings with valid css', t =>
-  stylelint.lint({
-    code: validScss,
-    config,
-    syntax: 'scss'
-  }).then(({errored, results}) => {
-    const {deprecations, invalidOptionWarnings, warnings} = results[0];
-    t.falsy(errored, 'no errored');
-    t.is(deprecations.length, 0, 'no deprecations');
-    t.is(invalidOptionWarnings.length, 0, 'no invalid option warnings');
-    t.is(warnings.length, 0, 'flags no warnings');
-  })
-);
+describe('no warnings with valid css', () => {
+  let result;
 
-test('a warning with invalid css', t =>
-  stylelint.lint({
-    code: invalidScss,
-    config,
-    syntax: 'scss'
-  }).then(({errored, results}) => {
-    const {deprecations, invalidOptionWarnings, warnings} = results[0];
-    t.truthy(errored, 'errored');
-    t.is(deprecations.length, 0, 'no deprecations');
-    t.is(invalidOptionWarnings.length, 0, 'no invalid option warnings');
-    t.is(warnings.length, 1, 'flags one warning');
-    t.is(warnings[0].text, 'Unexpected leading zero (number-leading-zero)', 'correct warning text');
-  })
-);
+  beforeEach(() => {
+    result = stylelint.lint({
+      code: validScss,
+      config,
+      syntax: 'scss'
+    });
+  });
+
+  it('no errored', () =>
+    result.then((data) => {
+      expect(data.errored).toBeFalsy();
+    }));
+
+  it('no deprecations', () =>
+    result.then((data) => {
+      expect(data.results[0].deprecations.length).toBe(0);
+    }));
+
+  it('no invalid option warnings', () =>
+    result.then((data) => {
+      expect(data.results[0].invalidOptionWarnings.length).toBe(0);
+    }));
+
+  it('flags no warnings', () =>
+    result.then((data) => {
+      expect(data.results[0].warnings.length).toBe(0);
+    }));
+});
+
+describe('a warning with invalid css', () => {
+  let result;
+
+  beforeEach(() => {
+    result = stylelint.lint({
+      code: invalidScss,
+      config,
+      syntax: 'scss'
+    });
+  });
+
+  it('errored', () =>
+    result.then((data) => {
+      expect(data.errored).toBeTruthy();
+    }));
+
+  it('no deprecations', () =>
+    result.then((data) => {
+      expect(data.results[0].deprecations.length).toBe(0);
+    }));
+
+  it('no invalid option warnings', () =>
+    result.then((data) => {
+      expect(data.results[0].invalidOptionWarnings.length).toBe(0);
+    }));
+
+  it('flags one warning', () =>
+    result.then((data) => {
+      expect(data.results[0].warnings.length).toBe(1);
+    }));
+
+  it('correct warning text', () =>
+    result.then((data) => {
+      expect(data.results[0].warnings[0].text).toBe(
+        'Unexpected leading zero (number-leading-zero)'
+      );
+    }));
+});
